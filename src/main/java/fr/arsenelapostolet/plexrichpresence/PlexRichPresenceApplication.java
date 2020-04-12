400ff0b97c4e8fec803327e8261f879def8ec60f
Arsène Lapostolet <arsene@lapostolet.fr>
2020-04-12 19:45:57 +0200

    initial commit

---
+++ src/main/java/fr/arsenelapostolet/plexrichpresence/PlexRichPresenceApplication.java
@@ -0,0 +1,13 @@
+package fr.arsenelapostolet.plexrichpresence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlexRichPresenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlexRichPresenceApplication.class, args);
+	}
+
+}
