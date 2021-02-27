9e3d254bc545e5187fcd37616ecab3839b6fd48a
Arsène Lapostolet <arsene@lapostolet.fr>
2021-02-27 22:41:37 +0100

    remove comments + add CDN

--- windows/chocolatey/plexrichpresence/tools/chocolateyinstall.ps1
+++ windows/chocolatey/plexrichpresence/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/Ombrelin/plex-rich-presence/releases/download/v1.3/plex-rich-presence-setup.exe' # download url, HTTPS preferred
+$url        = 'https://github.com/Ombrelin/plex-rich-presence/releases/download/v1.5/plex-rich-presence-setup-1.5.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -9,10 +9,6 @@ $packageArgs = @{
   url           = $url
   url64bit      = $url64
   softwareName  = 'plexrichpresence*'
-  checksum      = '8C307AF44E9CB8939EA9C7227A2305E08C1141FFAC50FC59096061BF8E8ED7F6'
-  checksumType  = 'sha256'
-  checksum64    = ''
-  checksumType64= 'sha256'
 
   silentArgs    = "/VERYSILENT"
   validExitCodes= @(0, 3010, 1641)
