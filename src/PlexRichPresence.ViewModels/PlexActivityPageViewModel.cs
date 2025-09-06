94f9055f373db9fc52beece2a8479decb17ff263
Ombrelin <arsene@lapostolet.fr>
2025-09-06 12:24:54 +0200

    Remove await on stopping rich presence

--- src/PlexRichPresence.ViewModels/PlexActivityPageViewModel.cs
+++ src/PlexRichPresence.ViewModels/PlexActivityPageViewModel.cs
@@ -85,7 +85,7 @@ public partial class PlexActivityPageViewModel : ObservableObject
 
                 if (CurrentActivity is "Idle" && !EnableIdleStatus)
                 {
-                    await discordService.StopRichPresence();
+                    discordService.StopRichPresence();
                 }
                 else
                 {
